8f8abe769adcedc6b2f5a93220d5a409ed2cf101
niemandkun <niemandkun@yandex.ru>
2016-01-17 22:31:33 +0500

    Sun Jan 17 22:31:33 YEKT 2016

---
+++ copyconfig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO=$(pwd)
+
+function copy {
+    if [ -e $HOME/$1 ]; then
+        echo "~/$1 --> $2"
+        cp "$HOME/$1" "$REPO/$2"
+    else
+        echo No such ~/$1. Skipping...
+    fi
+}
+
+
+echo -e "\nCopying config files to $REPO...\n"
+
+copy ".moc/config" "moc/config"
+
+copy ".config/i3/config" "i3/config"
+copy ".config/i3status/config" "i3/i3status"
+
+copy ".xinitrc" "dotfiles/xinitrc"
+copy ".Xresources" "dotfiles/Xresources"
+copy ".vimrc" "dotfiles/vimrc"
+copy ".zshrc" "dotfiles/zshrc"
+copy ".vimperatorrc" "dotfiles/vimperatorrc"
+copy ".gtkrc-2.0.mine" "dotfiles/vimperatorrc"
+copy ".gtkrc-2.mine" "dotfiles/vimperatorrc"
+
+echo -e "\nDone!"

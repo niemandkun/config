8caa417c22d8c039d9d627b6b38b82c0202ec238
niemandkun <niemandkun@niemandkun-laptop.Dlink>
2016-02-23 23:25:29 +0500

    Tue Feb 23 23:25:29 YEKT 2016

--- cpconfig.sh
+++ cpconfig.sh
@@ -24,6 +24,6 @@ copy ".Xresources" "dotfiles/Xresources"
 copy ".vimrc" "dotfiles/vimrc"
 copy ".zshrc" "dotfiles/zshrc"
 copy ".vimperatorrc" "dotfiles/vimperatorrc"
-copy ".gtkrc-2.0.mine" "dotfiles/gtkrc-2.0.mine"
+copy ".gtkrc-2.0" "dotfiles/gtkrc-2.0.mine"
 
 echo -e "\nDone!"
